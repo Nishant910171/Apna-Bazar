d61fe5173b4cddddcca6cefde5acea24b90181c2
Nishant Kumar <kumarnishantpradhan@gmail.com>
2025-01-15 16:02:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,10 +43,11 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
   res.json({
    success:1,
-   image_url:`http://localhost:${port}/images/${req.file.filename}`
+   image_url:`https://apna-bazar-backend.onrender.com/images/${req.file.filename}`
   })
 })
 
+//http://localhost:${port}
 // Schema for Creating Products
 
 const Product = mongoose.model("Product",{
